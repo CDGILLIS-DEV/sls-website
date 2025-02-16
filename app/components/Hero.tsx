55ec7d1db1034bade5b93b6debc05947d9667e2e
Carl Gillis <darnell@Carls-MacBook-Pro.local>
2025-02-16 03:30:13 -0500

    Changed the color of the font on home page.

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -7,7 +7,7 @@ const Hero = () => {
         <section className="relative flex flex-col justify-center items-center min-h-screen bg-light bg-cover bg-center bg-no-repeat h-screen"
             style={{ backgroundImage: "url(/truck_on_rainday.jpeg)" }}>
       <div className="container mx-auto text-center px-6 pb-20">
-        <h1 className="text-5xl md:text-5xl font-light text-primary leading-tight tracking-wide">
+        <h1 className="text-5xl md:text-5xl font-light text-[#64e687] leading-tight tracking-wide">
           Welcome to Simpatico Logistics Services
         </h1>
         <p className="mt-4 text-lg text-gray-600 max-w-2xl mx-auto pt-10">
