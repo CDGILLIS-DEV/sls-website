4b11de145875c2b16fb71e44f6b5ef31a58b3e5b
Carl Gillis <darnell@Carls-MacBook-Pro.local>
2025-02-17 14:48:30 -0500

    Created Firebase login auth  components

---
+++ app/context/AuthContext.tsx
@@ -0,0 +1,66 @@
+/* eslint-disable */
+import { Children, createContext, useContext, useEffect, useState } from "react";
+import { auth, googleProvider } from "@/lib/firbase";
+import { 
+    signInWithPopup,
+    signInWithEmailAndPassword,
+    createUserWithEmailAndPassword,
+    signOut,
+    onAuthStateChanged,
+    User
+ } from "firebase/auth";
+import { unsubscribe } from "diagnostics_channel";
+
+ interface AuthContextProps {
+    user: User | null;
+    loading: boolean;
+    signUp: (email: string, password: string) => Promise<void>;
+    login: (email: string, password: string) => Promise<void>;
+    googleLogin: () => Promise<void>;
+    logout: () => Promise<void>;
+ }
+
+ const AuthContext = createContext<AuthContextProps | undefined>(undefined);
+
+ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+    const [user, setUser] = useState<User | null>(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            setUser(user);
+            setLoading(false);
+        });
+        return () => unsubscribe();
+     }, []);
+
+     const signUp = async (email: string, password: string) => {
+        await createUserWithEmailAndPassword(auth, email, password);
+     };
+
+     const login = async (email: string, password: string) => {
+        signInWithEmailAndPassword(auth, email, password)
+     };
+
+     const googleLogin = async () => {
+        await signInWithPopup(auth, googleProvider)
+     };
+
+     const logout = async () => {
+        await signOut(auth);
+     };
+
+     return (
+        <AuthContext.Provider value={{ user, loading, signUp, login, googleLogin, logout }}>
+            {children}
+        </AuthContext.Provider>
+     );
+ };
+
+ export const useAuth = () => {
+    const context = useContext(AuthContext);
+    if (!context) {
+        throw new Error("useAuth must be used within an AuthProvider")
+    }
+    return context;
+ };
--- lib/firbase.ts
+++ lib/firbase.ts
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { GoogleAuthProvider } from "firebase/auth/web-extension";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,4 +18,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+
+export { auth, googleProvider };
