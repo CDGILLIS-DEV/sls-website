18476e704fa06076aab5653b36cc2c0920fa1513
Carl Gillis <darnell@Carls-MacBook-Pro.local>
2025-03-30 19:03:45 -0400

    Corrected some grammar

--- app/crm/page.tsx
+++ app/crm/page.tsx
@@ -5,7 +5,7 @@ export default function CRMPage() {
         <div className="min-h-screen bg-[#F9FAFB] p-8">
             <div className="max-w-5x1 mx-auto space-y-6">
             <h1 className="text 3x1 font-semibold text-gray-800">Simpatico CRM</h1>
-            <p className="text-gray-500 text-sm">Trak manufacturers you generate and manage shipment opportunities.</p>
+            <p className="text-gray-500 text-sm">Track manufacturers you generate and manage shipment opportunities.</p>
 
             <div className="bg-whit shadow-x1 rounded-2x1 p-6 transition hover:shadow-2x1">
                <LeadForm /> 
