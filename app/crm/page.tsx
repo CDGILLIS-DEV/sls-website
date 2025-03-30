84a4c91c5e67347bd7d96944d8eb6441080a3a6a
Carl Gillis <darnell@Carls-MacBook-Pro.local>
2025-03-30 17:54:43 -0400

    Made corrections to the /crm/pages.tsx file

---
+++ app/crm/page.tsx
@@ -0,0 +1,8 @@
+export default function CRMPage() {
+    return (
+        <div className="p-8">
+            <h1 className="text 2x1 font-bold">Simpatico CRM</h1>
+            <p className="text-gray-600 mt-2">This is the start of your custom CRM dashboard</p>
+        </div>
+    );
+}
