7fc5e35a094dd317a488c6773bcb283caa4628b6
Carl Gillis <darnell@Carls-MacBook-Pro.local>
2025-03-30 18:17:14 -0400

    Activated the crm page w/ LeadForm component rendering

--- app/crm/page.tsx
+++ app/crm/page.tsx
@@ -1,8 +1,16 @@
+import LeadForm from "./components/LeadForm";
+
 export default function CRMPage() {
     return (
-        <div className="p-8">
-            <h1 className="text 2x1 font-bold">Simpatico CRM</h1>
-            <p className="text-gray-600 mt-2">This is the start of your custom CRM dashboard</p>
+        <div className="min-h-screen bg-[#F9FAFB] p-8">
+            <div className="max-w-5x1 mx-auto space-y-6">
+            <h1 className="text 3x1 font-semibold text-gray-800">Simpatico CRM</h1>
+            <p className="text-gray-500 text-sm">Trak manufacturers you generate and manage shipment opportunities.</p>
+
+            <div className="bg-whit shadow-x1 rounded-2x1 p-6 transition hover:shadow-2x1">
+               <LeadForm /> 
+            </div>
+            </div>
         </div>
     );
 }
